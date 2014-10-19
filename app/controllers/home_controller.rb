6debcb5a9191efbf38ee8fdc2a5b218ba553af38
Benjamin Huber <benjamin@rubberandglue.at>
2014-10-19 13:42:15 +0200

    search field for ingredients

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,4 +3,21 @@ class HomeController < ApplicationController
     @ingredients = Ingredient.all
     @recipes     = Recipe.all
   end
+
+  def ingredient_list
+    @ingredients = Ingredient.where ['name LIKE ?', "%#{params[:q]}%"]
+    respond_to do |format|
+      format.json do
+        render json: @ingredients
+      end
+    end
+  end
+
+  def recipes
+
+  end
+
+  def possible_ingredients
+
+  end
 end
